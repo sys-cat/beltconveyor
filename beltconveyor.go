d5beae902818cd7fc8862cd12fc2209d8a2fd381
sys-cat <systemcat91@gmail.com>
2019-03-11 03:43:35 +0900

    upodate main file, add png convert file

--- beltconveyor.go
+++ beltconveyor.go
@@ -1,5 +1,43 @@
 package beltconveyor
 
-func beltconveyor() bool {
-	return true
+import (
+	"image"
+	"os"
+)
+
+type (
+	Beltconveyor struct {
+		Config    image.Config
+		Extension string
+		ReadPath  string
+		File      *os.File
+	}
+)
+
+func New(path string) Beltconveyor {
+	return Beltconveyor{
+		ReadPath: path,
+	}
+}
+
+func (b *Beltconveyor) Open() (err error) {
+	b.File, err = os.Open(b.ReadPath)
+	return err
+}
+
+func (b *Beltconveyor) Decode() (err error) {
+	b.Config, b.Extension, err = image.DecodeConfig(b.File)
+	return err
+}
+
+func (b *Beltconveyor) ConvertPNG() (err error) {
+	return err
+}
+
+func (b *Beltconveyor) ConvertJPG() (err error) {
+	return err
+}
+
+func (b *Beltconveyor) ConvertWebp() (err error) {
+	return err
 }
---
+++ png.go
@@ -0,0 +1,5 @@
+package beltconveyor
+
+func Convert() {
+	return
+}
