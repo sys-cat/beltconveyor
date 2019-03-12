91b9e7febad1d4a631f357429d6c7f3c490c20c9
sys-cat <systemcat91@gmail.com>
2019-03-13 02:40:09 +0900

    debuging

--- cmd/beltconveyor/main.go
+++ cmd/beltconveyor/main.go
@@ -8,7 +8,15 @@ import (
 )
 
 func main() {
-	item := beltconveyor.New("")
+	item := beltconveyor.New("../output/test.png")
+	if err := item.Open; err != nil {
+		fmt.Printf("Open faild, %s", err)
+		os.Exit(1)
+	}
+	if err := item.Decode; err != nil {
+		fmt.Printf("Decode faild, %s", err)
+		os.Exit(1)
+	}
 	fmt.Printf("%#v", item)
 
 	os.Exit(0)
